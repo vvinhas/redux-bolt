774cb87d091d0febcb59166aa67824935dc55903
Vinicius Vinhas <vinhas@cdts.fiocruz.br>
2019-05-14 00:32:14 -0300

    Writing a few middleware tests

--- src/createBoltMiddleware.test.js
+++ src/createBoltMiddleware.test.js
@@ -1,9 +1,65 @@
+import { types, events } from './constants'
 import createBoltMiddleware from './createBoltMiddleware'
 
+const mocks = {
+  // WS Mock
+  socket: {
+    connected: true,
+    emit: jest.fn(),
+    on: jest.fn()
+  },
+  // Redux Mock
+  store: {
+    dispatch: jest.fn()
+  },
+  next: jest.fn(),
+  // Bolt Custom Options
+  options: {
+    propName: 'socket',
+    queueManager: {
+      push: jest.fn(),
+      release: jest.fn()
+    }
+  }
+}
+
 describe('createBoltMiddleware', () => {
-  it('Should use default options when no options are defined', () => {})
-  it('Should be able to let the developer change the propName option', () => {})
-  it('Should return a valid Redux middleware', () => {})
+  afterEach(() => {
+    jest.resetAllMocks()
+  })
+  it('Should return a valid Redux middleware', () => {
+    const { socket, store, next, options } = mocks
+    createBoltMiddleware(socket, options)(store)(next)({
+      type: 'ACTION'
+    })
+    expect(next.mock.calls.length).toBe(1)
+  })
+
+  it('Should let the developer change the propName option', () => {
+    const { socket, store, next, options } = mocks
+    createBoltMiddleware(socket, options)(store)(next)({
+      type: 'MY_ACTION',
+      socket: true
+    })
+    expect(next.mock.calls.length).toBe(1)
+    expect(next.mock.calls[0][0]).toEqual({
+      type: 'MY_ACTION',
+      socket: {
+        type: types.send,
+        event: events.message
+      }
+    })
+  })
+
+  it("Should'nt handle actions other than Bolt actions", () => {
+    const { socket, store, next, options } = mocks
+    createBoltMiddleware(socket, options)(store)(next)({
+      type: 'REGULAR_REDUX_ACTION'
+    })
+    expect(options.queueManager.push.mock.calls.length).toBe(0)
+  })
+
+  it('Should push every Bolt action to the queue manager', () => {})
   it("Should register Bolt's default handlers", () => {})
   it("Should let the developer register it's own handlers", () => {})
   it('Should thrown an exception when the handler has no event nor handler', () => {})
