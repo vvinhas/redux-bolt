62d50179f4a959666fb6d9b3be38aeb42a1f0903
Vinicius Vinhas <vinhas@cdts.fiocruz.br>
2019-05-13 23:59:47 -0300

    Beginning createBoltMiddleware unit test

---
+++ src/createBoltMiddleware.test.js
@@ -0,0 +1,14 @@
+import createBoltMiddleware from './createBoltMiddleware'
+
+describe('createBoltMiddleware', () => {
+  it('Should use default options when no options are defined', () => {})
+  it('Should be able to let the developer change the propName option', () => {})
+  it('Should return a valid Redux middleware', () => {})
+  it("Should register Bolt's default handlers", () => {})
+  it("Should let the developer register it's own handlers", () => {})
+  it('Should thrown an exception when the handler has no event nor handler', () => {})
+  it('Should register the handlers to the socket', () => {})
+  it('Should convert a boolean value to a valid Bolt object', () => {})
+  it('Should ignore Bolt/Receive actions', () => {})
+  it("Should not release an action when the socket isn't connected", () => {})
+})
